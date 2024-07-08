29d656168d6633befea91a308ccfad32334d99f1
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-07-08 15:41:55 +0500

    Done InboxView
    added scroll active now view, and added inboxRowView

---
+++ FriendsChat/FriendsChat/Core/Inbox/InboxView.swift
@@ -0,0 +1,53 @@
+//
+//  InboxView.swift
+//  FriendsChat
+//
+//  Created by Иван Незговоров on 08.07.2024.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ActiveNowView()
+                
+                List {
+                    ForEach(0 ... 10, id: \.self) { message in
+                        InboxRowView()
+                    }
+                }
+                .listStyle(PlainListStyle())
+                .frame(height: UIScreen.main.bounds.height - 100)
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("Show new message view")
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    InboxView()
+}
