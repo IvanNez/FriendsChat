9d6ee164d8443b65a5a934f65948616673bfbf39
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-07-08 13:37:46 +0500

    init project

---
+++ FriendsChat/FriendsChat/FriendsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsChatApp.swift
+//  FriendsChat
+//
+//  Created by Иван Незговоров on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
